c57e2f582ec99052612ece6967bd877b982a1295
Tarek Galal <tare2.galal@gmail.com>
2015-03-25 09:54:41 +0100

    Wifi submaker initial commit

--- inception/argparsers/mk/makers/submakers/submaker_wifi.py
+++ inception/argparsers/mk/makers/submakers/submaker_wifi.py
@@ -5,8 +5,24 @@ class WifiSubmaker(Submaker):
     def make(self, workDir):
         aps = self.getConfigValue("aps")
 
+
+        if not len(aps):
+            return
+
+        wpaSupplicantData= {
+            "destination": "/data/misc/wifi/wpa_supplicant.conf",
+            "mode": "0660",
+            "gid": "1010",
+            "uid": "1000"
+        }
+
+        self.setConfigValue("update.files.add.data/misc/wifi/wpa_supplicant\.conf", wpaSupplicantData)
+
+
         gen = WPASupplicantConfGenerator()
         gen.setWorkDir(workDir)
+
+
 
         for ap in aps:
             ssid = ap["ssid"]
