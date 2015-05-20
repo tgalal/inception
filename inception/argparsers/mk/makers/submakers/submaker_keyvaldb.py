7f00ce6d92319aa72e7f9ea5d76394331adf3d8b
Tarek Galal <tare2.galal@gmail.com>
2015-05-20 14:33:28 +0200

    More flexible keyvaldb implementation

--- inception/argparsers/mk/makers/submakers/submaker_keyvaldb.py
+++ inception/argparsers/mk/makers/submakers/submaker_keyvaldb.py
@@ -4,15 +4,32 @@ import sqlite3
 class KeyValDBSubmaker(Submaker):
     def __init__(self, *args):
         super(KeyValDBSubmaker, self).__init__(*args)
+        self.locale = "en_US"
+
+    def setLocale(self, locale):
+        self.locale = locale
 
     def apply(self, dbPath, tableName, keyvals, colId = "_id", colKey = "name", colVal = "value"):
         conn = sqlite3.connect(dbPath)
         conn.text_factory = str
         table = Table(tableName, colId, colKey, colVal)
+        xtable = XTable(tableName)
+        xtable.setPKColumn(colId)
+        xtable.addColumn(colKey, str)
+        xtable.addColumn(colVal, str)
+        xtable.addIndex(colKey, tableName + "Index1")
+        xtable.create()
         print("Creating %s:\t%s" % (dbPath, tableName))
         for key, val in keyvals.items():
             table.insert(key, val)
-        table.execute(conn)
+            kwargs = {
+                colKey: key,
+                colVal: val
+            }
+            xtable.insert(**kwargs)
+
+        #table.execute(conn)
+        xtable.execute(conn)
         conn.commit()
         conn.close()
 
@@ -42,6 +59,120 @@ class DeleteQuery(QueryWithParams):
 
 class InsertQuery(QueryWithParams):
     pass
+
+
+class Column(object):
+
+        _TYPE_MAP = {
+            int: "INTEGER",
+            str: "TEXT"
+        }
+
+        def __init__(self, name, typ):
+            self.name = name
+            self.typ = typ
+            assert typ in self.__class__._TYPE_MAP, "%s is not supported" % typ
+            self.unique = False
+            self.conflictBehavior = "REPLACE"
+            self.autoincrement = False
+            self.pk = False
+
+        def setPrimaryKey(self, pk):
+            self.pk = pk
+
+        def isPrimaryKey(self):
+            return self.pk
+
+        def isAutoIncerment(self):
+            return self.autoincrement
+
+        def setAutoIncrement(self, autoIncrement):
+            if autoIncrement and not self.typ is int:
+                raise Exception("CAN'T AUTOINCREMENT NON INT TYPE")
+            self.autoincrement = autoIncrement
+
+        def setUnique(self, conflict = "REPLACE"):
+            self.unique = True
+            self.conflictBehavior = conflict
+
+        def unsetUnique(self):
+            self.unique = False
+
+        def isUnique(self):
+            return self.unique
+
+        def __str__(self):
+            colStr = "{name} {typ} {pk} {autoincr} {unique}".format(
+                name = self.name,
+                typ = self.__class__._TYPE_MAP[self.typ],
+                pk = "PRIMARY KEY"  if self.isPrimaryKey() else "",
+                autoincr = "AUTOINCREMENT" if self.autoincrement else "",
+                unique = ("UNIQUE ON CONFLICT %s" % self.conflictBehavior) if self.isUnique() else ""
+            )
+
+            return colStr
+
+class PrimaryKeyColumn(Column):
+    def __init__(self, name):
+        super(PrimaryKeyColumn, self).__init__(name, int)
+        self.setAutoIncrement(True)
+        self.setPrimaryKey(True)
+
+class XTable(object):
+    _CREATE_QUERY = 'CREATE TABLE IF NOT EXISTS {table} ({cols});'
+    _INDEX_QUERY =  'CREATE INDEX IF NOT EXISTS {index_name} ON {table} ({column});'
+    _INSERT_QUERY = 'INSERT INTO {table} ({cols}) VALUES ({vals})'
+
+    def __init__(self, name):
+        self.name = name
+        self.primaryKey = None
+        self.columns = []
+        self.indices = {}
+        self.queries = []
+
+
+    def __str__(self, separator = "\n"):
+        return separator.join([q.__str__() for q in self.queries])
+
+    def setPKColumn(self, name):
+        self.primaryKey = PrimaryKeyColumn(name)
+
+    def addColumn(self, name, typ):
+        self.columns.append(Column(name, typ))
+
+    def addIndex(self, colName, indexName):
+        for c in self.columns:
+            if c.name == colName:
+                self.indices[colName] = indexName
+                return
+        raise Exception("Column %s is not in table, can't set index" % colName)
+
+    def create(self):
+        cols = [col.__str__() for col in self.columns]
+        if self.primaryKey:
+            cols.insert(0, self.primaryKey.__str__())
+
+        colsStr = ", ".join(cols)
+        createQuery = self.__class__._CREATE_QUERY.format(table = self.name, cols = colsStr)
+        self.queries.append(Query(createQuery))
+
+        for colName, indexName in self.indices.items():
+            indexQuery = self.__class__._INDEX_QUERY.format(index_name = indexName, column = colName, table = self.name)
+            self.queries.append(Query(indexQuery))
+
+    def insert(self, **kwargs):
+        cols = kwargs.keys()
+        colsStr = ", ".join(cols)
+        argsStr = ", " .join(["?" for i in cols])
+        preFormatedInsertQuery = self._INSERT_QUERY.format(cols = colsStr, vals = argsStr, table = self.name)
+        self.queries.append(InsertQuery(preFormatedInsertQuery, (kwargs.values())))
+
+    def execute(self, conn):
+        for query in self.queries:
+            query.execute(conn)
+
+# class MetadataTable(object):
+#     _CREATE_QUERY = 'CREATE TABLE IF NOT EXISTS android_metadata (locale TEXT);'
 
 class Table(object):
     _CREATE_QUERY = 'CREATE TABLE IF NOT EXISTS {table} ({column_id} INTEGER PRIMARY KEY AUTOINCREMENT,{column_key} TEXT UNIQUE ON CONFLICT REPLACE,{column_val} TEXT);'
@@ -85,6 +216,6 @@ class Table(object):
            column_key = self.colKey,
         )
 
-        self.queries.append(InsertQuery(insertQuery, (str(self.id_), key, val)))
         self.queries.append(DeleteQuery(deleteQuery, (key,)))
+        self.queries.append(InsertQuery(insertQuery, (str(self.id_), key, val)))
         self.id_ += 1
