9c41a0c0669706bd1d02e752750bd068915761bd
Tarek Galal <tare2.galal@gmail.com>
2015-05-26 11:25:55 +0200

    Adb keys gen initial commit

--- inception/argparsers/mk/makers/maker_update.py
+++ inception/argparsers/mk/makers/maker_update.py
@@ -5,23 +5,29 @@ from .submakers.submaker_updatescript import UpdatescriptSubmaker
 from .submakers.submaker_keyvaldb_settings import SettingsKeyValDBSubmaker
 from .submakers.submaker_property import PropertySubmaker
 from .submakers.submaker_updatezip import UpdatezipSubmaker
+from .submakers.submaker_adbkeys import AdbKeysSubmaker
 import shutil
 import os
+import logging
+logger = logging.getLogger(__name__)
 class UpdateMaker(Maker):
     def __init__(self, config):
         super(UpdateMaker, self).__init__(config, "update")
         self.rootFs = "fs"
 
     def make(self, workDir, outDir):
+        logger.info("Making update package")
         rootFS = os.path.join(workDir, self.rootFs)
         self.makeFS(rootFS)
         self.makeProps(rootFS)
         self.makeWPASupplicant(rootFS)
         self.makeSettings(rootFS)
+        self.makeAdbKeys(rootFS)
         self.makeUpdateScript(rootFS)
         self.makeUpdateZip(rootFS, outDir)
 
     def makeFS(self, fsPath):
+        logger.info("Making FS")
         if not os.path.exists(fsPath):
             os.makedirs(fsPath)
 
@@ -29,23 +35,33 @@ class UpdateMaker(Maker):
         smaker.make(fsPath)
 
     def makeSettings(self, workDir):
+        logger.info("Making Settings databases")
         smaker = SettingsKeyValDBSubmaker(self, "settings")
         smaker.make(workDir)
 
     def makeWPASupplicant(self, workDir):
+        logger.info("Making WPASupplicant")
         wpaSupplicantDir = os.path.join(workDir, "data", "misc", "wifi")
         smaker = WifiSubmaker(self, "network")
         smaker.make(wpaSupplicantDir)
 
     def makeUpdateScript(self, updatePkgDir):
+        logger.info("Making Update script")
         smaker = UpdatescriptSubmaker(self, ".")
         smaker.make(updatePkgDir)
 
     def makeProps(self, workDir):
+        logger.info("Making /data/property")
         smaker = PropertySubmaker(self, "property")
         smaker.make(workDir)
 
+    def makeAdbKeys(self, workDir):
+        logger.info("Making ADB keys")
+        smaker = AdbKeysSubmaker(self, "adb")
+        smaker.make(workDir)
+
     def makeUpdateZip(self, work, outDir):
+        logger.info("Making Update zip")
         smake = UpdatezipSubmaker(self, ".")
         updateZipPkgPath = smake.make(work)
         shutil.copy(updateZipPkgPath, outDir)
---
+++ inception/argparsers/mk/makers/submakers/submaker_adbkeys.py
@@ -0,0 +1,22 @@
+from .submaker import Submaker
+import os
+class AdbKeysSubmaker(Submaker):
+    PATH_DEVICE_ADBKEYS = "data/misc/adb/adb_keys"
+    def make(self, workDir):
+        adbKeys = self.getConfigValue("keys", [])
+        outData = ""
+        outPath = os.path.join(workDir, self.__class__.PATH_DEVICE_ADBKEYS)
+        for key in adbKeys:
+            outData += key + "\n"
+
+        os.makedirs(os.path.dirname(outPath))
+        with open(outPath, "w") as f:
+            f.write(outData)
+
+        self.setConfigValue("update.files.add.data/misc/adb", {
+                "destination": "/data/misc/adb",
+                "uid": "1000",
+                "gid": "2000",
+                "mode": "0600",
+                "mode_dirs": "0640"
+            })
