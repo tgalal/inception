a8175eb5e02bee7e3a919dd6e8bac799ca0de33e
Tarek Galal <tare2.galal@gmail.com>
2015-05-27 14:00:14 +0200

    Extract files/dirs based on config rather than fs dir

--- inception/argparsers/mk/makers/submakers/submaker_updatescript.py
+++ inception/argparsers/mk/makers/submakers/submaker_updatescript.py
@@ -22,33 +22,49 @@ class UpdatescriptSubmaker(Submaker):
             u.rm("/data", recursive=True)
 
         #### extract our FS
-        for f in os.listdir(updatePkgDir):
-            target = "/" + f
-            if os.path.isdir(os.path.join(updatePkgDir,f)):
-                u.extractDir(target[1:], target)
-            else:
-                u.extractFile(target[1:], target)
+        # for f in os.listdir(updatePkgDir):
+        #     target = "/" + f
+        #     if os.path.isdir(os.path.join(updatePkgDir,f)):
+        #         u.extractDir(target[1:], target)
+        #     else:
+        #         u.extractFile(target[1:], target)
+
+
 
         #### apply our FS permissions
 
-
+        extractedDirs = []
         for path, pathData in self.getConfigValue("files.add", {}).items():
             destPath = pathData["destination"]
             if not destPath.startswith("/"):
                 destPath = "/" + destPath
 
-            localPath = updatePkgDir + destPath
+            if path.startswith("/"):
+                path = path[1:]
+
+            localPath = updatePkgDir + "/" + path
 
             if "__depend__" in pathData:
                 if self.getMaker().getConfig().get(pathData["__depend__"]) is None:
                     continue
             if not os.path.exists(localPath):
-                    raise Exception("Cannot set permissions for a missing file: " + destPath)
+                    raise Exception("Cannot set permissions for a missing file: " + localPath)
+
+
+            if os.path.isdir(localPath):
+                u.extractDir(path, destPath)
+                extractedDirs.append(destPath)
+            else:
+                dirname = os.path.dirname(destPath)
+                if dirname not in extractedDirs:
+                    u.extractDir(os.path.dirname(path), dirname)
+                    extractedDirs.append(dirname)
+                u.extractFile(path, destPath)
+
 
             permState = ("mode" in pathData, "uid" in pathData, "gid" in pathData)
             if any(permState):
                 if not all(permState):
-                    print(pathData)
                     raise Exception("Missing permission data for " + destPath)
 
                 if "mode_dirs" in pathData:
@@ -86,7 +102,7 @@ class UpdatescriptSubmaker(Submaker):
             if preScript:
                 updateScriptFile.write(preScript)
                 updateScriptFile.write("\n")
-            updateScriptFile.write(u.generate())
+            updateScriptFile.write(u.generate(showProgress=self.getConfigValue("script.progress", True)))
 
             if postScript:
                 updateScriptFile.write("\n")
