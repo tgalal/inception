33b2793c2c8ffa87cde64c0cc203c80ea91386d9
Tarek Galal <tare2.galal@gmail.com>
2015-05-28 11:14:04 +0200

    setup.py initial commit

--- inception/__init__.py
+++ inception/__init__.py
@@ -2,3 +2,6 @@ from inceptionobject import InceptionObject, InceptionExecCmdFailedException
 from constants import InceptionConstants
 from configurator import Configurator, ConfigNotFoundException, ConfigNotValidException
 from tools import Heimdall, RkFlashTool
+
+__version__ = "0.1"
+__author__ = "Tarek Galal"
---
+++ setup.py
@@ -0,0 +1,27 @@
+deps = ['dulwich', 'argparse']
+from setuptools import setup, find_packages
+import inception
+setup(
+    name='inception-android',
+    version=inception.__version__,
+    url='http://github.com/tgalal/yowsup/',
+    license='GPL-3+',
+    author='Tarek Galal',
+    tests_require=[],
+    install_requires = deps,
+    scripts = ['incept'],
+    author_email='tare2.galal@gmail.com',
+    description='Auto config tools for android devices',
+    #long_description=long_description,
+    packages= find_packages(),
+    include_package_data=True,
+    platforms='linux',
+    #test_suite='',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux'
+        ]
+)
