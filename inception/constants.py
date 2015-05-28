621c4055d2620a254a610ad08ef74ce834596f24
Tarek Galal <tare2.galal@gmail.com>
2015-05-28 11:14:40 +0200

    sane dirs in constants

--- inception/constants.py
+++ inception/constants.py
@@ -1,8 +1,14 @@
+import os
 class InceptionConstants:
-    VARIANTS_DIR = "./device"
-    BASE_DIR = "./base"
-    FS_DIR  = "fs"
-    WORK_DIR = "./work"
-    OUT_DIR = "./out"
+    INCEPTION_DIR = os.path.expanduser("~/.inception")
+    VARIANTS_DIR = os.path.join(INCEPTION_DIR, "variants")
+    BASE_DIR = os.path.join(INCEPTION_DIR, "base")
+    WORK_DIR = os.path.join(INCEPTION_DIR, "work")
+    OUT_DIR = os.path.join(INCEPTION_DIR, "out")
     OUT_NAME_UPDATE = "update.zip"
     OUT_NAME_RECOVERY = "recovery.img"
+    FS_DIR  = "fs"
+    LOOKUP_REPOS = [
+        "https://github.com/tgalal",
+        "https://github.com/inception-android"
+    ]
