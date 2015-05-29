a2f9ad1e7b7d0f8ba13823ac7375b8628e6588af
Tarek Galal <tare2.galal@gmail.com>
2015-05-29 11:57:08 +0200

    Check cache.size set before cache create

--- inception/argparsers/mk/makers/maker_cache.py
+++ inception/argparsers/mk/makers/maker_cache.py
@@ -1,6 +1,7 @@
 from .maker import Maker
 from inception.generators.cacheimg import CacheImgGenerator
 import os
+from inception.constants import InceptionConstants
 
 class CacheMaker(Maker):
     def __init__(self, config):
@@ -9,6 +10,7 @@ class CacheMaker(Maker):
     def make(self, workDir, outDir):
         make_ext4fsBin = self.getCommonConfigValue("tools.make_ext4fs.bin")
         cacheSize = self.getMakeConfigValue("size")
+        assert cacheSize, "cache.size is not set, can't create cache img"
         cacheSparsed= self.getMakeConfigValue("sparsed", False)
         cacheMount = self.getMakeConfigValue("mount", "cache")
         cacheMount = cacheMount[1:] if cacheMount[0] == "/" else cacheMount
@@ -19,7 +21,7 @@ class CacheMaker(Maker):
         gen.setSize(cacheSize)
         gen.setSparsed(cacheSparsed)
         gen.setMountPoint(cacheMount)
-        updatePkgPath = os.path.join(outDir, "update.zip")
+        updatePkgPath = os.path.join(outDir, InceptionConstants.OUT_NAME_UPDATE)
         if os.path.exists(updatePkgPath):
             gen.update(updatePkgPath)
-        gen.generate(os.path.join(outDir, "cache.img"))
+        gen.generate(os.path.join(outDir, InceptionConstants.OUT_NAME_CACHE))
