d4f85f48dec2b8131db2bdc2474d9f66b9a7d998
Tarek Galal <tare2.galal@gmail.com>
2015-06-10 02:59:19 +0200

    Optional track time per argparser

--- inception/argparsers/argparser.py
+++ inception/argparsers/argparser.py
@@ -3,6 +3,10 @@ from inception.inceptionobject import InceptionObject
 class InceptionArgParser(argparse.ArgumentParser, InceptionObject):
     def __init__(self, *args, **kwargs):
         super(InceptionArgParser, self).__init__(*args, **kwargs)
+        self.time = True
+
+    def trackTime(self):
+        return self.time
 
     def getArgs(self):
         return self.parse_args()
