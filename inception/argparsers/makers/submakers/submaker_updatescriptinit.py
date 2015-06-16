28d610ea83930d7555fd6c0737dcbd41e958bbc1
Tarek Galal <tare2.galal@gmail.com>
2015-06-16 23:37:49 +0200

    Use edify mount command instead of mount bin

--- inception/argparsers/makers/submakers/submaker_updatescriptinit.py
+++ inception/argparsers/makers/submakers/submaker_updatescriptinit.py
@@ -3,8 +3,14 @@ class UpdatescriptInitSubmaker(Submaker):
     def make(self, updatePkgDir, updatescriptGen = None):
         updatescriptGen.setVerbose(self.getValue("script.verbose", True))
         updatescriptGen.setVerbose(self.getValue("script.verbose", True))
+        mountNames= ["system", "data"]
 
-        #### mount needed FS
-        updatescriptGen.mount("/system")
-        updatescriptGen.mount("/data")
+        for mountName in mountNames:
+            dev = self.getMaker().getConfig().getMountConfig(mountName + ".dev")
+            type_ = self.getMaker().getConfig().getMountConfig(mountName + ".type", "EMMC")
+            fs = self.getMaker().getConfig().getMountConfig(mountName + ".fs", "ext4")
+            mountPoint = self.getMaker().getConfig().getMountConfig(mountName + ".mount", "/" + mountName)
 
+            assert dev, "__config__.target.mount.%s.dev is not set" % mountName
+
+            updatescriptGen.mount(dev, mountPoint,  fs, type_)
