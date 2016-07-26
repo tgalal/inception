961fd4d7de859af487981e97d25f0f0b639ea57e
Tarek Galal <tarek.galal@suitepad.de>
2016-07-26 15:29:47 +0200

    Handle extras.files

--- inception/argparsers/makers/maker_extras.py
+++ inception/argparsers/makers/maker_extras.py
@@ -22,4 +22,19 @@ class ExtrasMaker(Maker):
             shutil.copy(partitions.resolveRelativePath(path), imgTargetPath)
             result.append(imgTargetPath)
 
+        files = self.getMakeValue("files", {})
+        for src, dest in files.items():
+            fileTargetPath = os.path.join(outDir, dest)
+            if not os.path.exists(os.path.dirname(fileTargetPath)):
+                os.makedirs(os.path.dirname(fileTargetPath))
+
+            srcSource = self.getMakeProperty("files.%s" % src.replace(".", "\."))
+            realSourcePath = srcSource.resolveRelativePath(src)
+
+
+            logger.info("Copying %s -> %s" % (realSourcePath, dest))
+            shutil.copy(realSourcePath, fileTargetPath)
+            result.append(fileTargetPath)
+
+
         return "\n\t".join(result)
