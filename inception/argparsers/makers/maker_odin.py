9a6fe18e8db950fecea29956e7dbc936b571a448
Tarek Galal <tarek.galal@suitepad.de>
2016-08-09 13:52:32 +0200

    Handle odin.flatten odin.out
    Output odin files in the specified odin.out dir
    odin.flatted to create a single flat odin package

--- inception/argparsers/makers/maker_odin.py
+++ inception/argparsers/makers/maker_odin.py
@@ -7,7 +7,10 @@ class OdinMaker(Maker):
     def __init__(self, config):
         super(OdinMaker, self).__init__(config, "odin")
     def make(self, workDir, outDir):
-
+        odinOutDir = os.path.join(outDir, self.config.getOdinOutPath())
+        if not os.path.exists(odinOutDir):
+            os.makedirs(odinOutDir)
+        flatten = self.getMakeValue("flatten", False)
         includes = {
             "CSC": [
                 os.path.join(outDir, self.getCacheOutName())
@@ -32,29 +35,40 @@ class OdinMaker(Maker):
             includes["BL"].append(blProp.resolveRelativePath(blFile))
 
         resultTars = []
+        tarTmps = []
 
         for key, paths in includes.items():
             if not len(paths):
                 continue
-            outTarPath = os.path.join(outDir, key + "_" + InceptionConstants.OUT_NAME_ODIN.format(identifier = self.config.getIdentifier().replace(".", "-")))
-            checksummedOutTar = outTarPath + ".md5"
-            with tarfile.TarFile(outTarPath, "w", format = tarfile.USTAR_FORMAT) as outTarFile:
+
+            if flatten:
+                outTarPath = os.path.join(odinOutDir, InceptionConstants.OUT_NAME_ODIN.format(identifier = self.config.getIdentifier().replace(".", "-")))
+            else:
+                outTarPath = os.path.join(odinOutDir, key + "_" + InceptionConstants.OUT_NAME_ODIN.format(identifier = self.config.getIdentifier().replace(".", "-")))
+
+            with tarfile.TarFile(outTarPath, "a", format = tarfile.USTAR_FORMAT) as outTarFile:
                 for path in paths:
                     if os.path.exists(path):
                         outTarFile.add(path, os.path.basename(path))
                     else:
                         raise ValueError("%s does not exist" % path)
 
-            if self.getMakeValue("checksum", True):
-                with open(outTarPath, "rb") as outTarFile:
+
+
+            tarTmps.append(outTarPath)
+
+        if self.getMakeValue("checksum", True):
+            for tarTmp in set(tarTmps):
+                checksummedOutTar = tarTmp + ".md5"
+                with open(tarTmp, "rb") as outTarFile:
                     md5sum = hashlib.md5(outTarFile.read()).hexdigest()
 
-                with open(outTarPath, "ab") as outTarFile:
-                    outTarFile.write(md5sum + "  " + os.path.basename(outTarPath) + "\n")
+                with open(checksummedOutTar, "ab") as outTarFile:
+                    outTarFile.write(md5sum + "  " + os.path.basename(tarTmp) + "\n")
 
-                os.rename(outTarPath, checksummedOutTar)
-
-            resultTars.append(checksummedOutTar)
-
+                os.rename(tarTmp, checksummedOutTar)
+                resultTars.append(checksummedOutTar)
+        else:
+            resultTars.extend(tarTmps)
 
         return resultTars
