c8252427cef4bd0a901a627fbeeaf1bb3e0a89a3
Tarek Galal <tarek.galal@suitepad.de>
2016-08-09 13:16:07 +0200

    Allow shipping CSC, AP, BL images in odin config

--- inception/argparsers/makers/maker_odin.py
+++ inception/argparsers/makers/maker_odin.py
@@ -7,29 +7,54 @@ class OdinMaker(Maker):
     def __init__(self, config):
         super(OdinMaker, self).__init__(config, "odin")
     def make(self, workDir, outDir):
-        allIncludes = [
-            self.getCacheOutName(),
-            InceptionConstants.OUT_NAME_RECOVERY,
-            InceptionConstants.OUT_NAME_BOOT
-        ]
 
-        outTarPath = os.path.join(outDir, InceptionConstants.OUT_NAME_ODIN.format(identifier = self.config.getIdentifier().replace(".", "-")))
-        checksummedOutTar = outTarPath + ".md5"
-        with tarfile.TarFile(outTarPath, "w", format = tarfile.USTAR_FORMAT) as outTarFile:
-            for inc in allIncludes:
-                incPath = os.path.join(outDir, inc)
-                if os.path.exists(incPath):
-                    outTarFile.add(incPath, os.path.basename(incPath))
+        includes = {
+            "CSC": [
+                os.path.join(outDir, self.getCacheOutName())
+            ],
+            "AP": [
+                os.path.join(outDir, InceptionConstants.OUT_NAME_RECOVERY)
+            ],
+            "BL": []
+        }
 
-        if self.getMakeValue("checksum", True):
-            with open(outTarPath, "rb") as outTarFile:
-                md5sum = hashlib.md5(outTarFile.read()).hexdigest()
+        cscProp = self.getMakeProperty("CSC", [])
+        apProp = self.getMakeProperty("AP", [])
+        blProp = self.getMakeProperty("BL", [])
 
-            with open(outTarPath, "ab") as outTarFile:
-                outTarFile.write(md5sum + "  " + os.path.basename(outTarPath) + "\n")
+        for cscFile in cscProp.value:
+            includes["CSC"].append(cscProp.resolveRelativePath(cscFile))
 
-            os.rename(outTarPath, checksummedOutTar)
+        for apFile in apProp.value:
+            includes["AP"].append(apProp.resolveRelativePath(apFile))
 
-            return checksummedOutTar
+        for blFile in blProp.value:
+            includes["BL"].append(blProp.resolveRelativePath(blFile))
 
-        return outTarPath
+        resultTars = []
+
+        for key, paths in includes.items():
+            if not len(paths):
+                continue
+            outTarPath = os.path.join(outDir, key + "_" + InceptionConstants.OUT_NAME_ODIN.format(identifier = self.config.getIdentifier().replace(".", "-")))
+            checksummedOutTar = outTarPath + ".md5"
+            with tarfile.TarFile(outTarPath, "w", format = tarfile.USTAR_FORMAT) as outTarFile:
+                for path in paths:
+                    if os.path.exists(path):
+                        outTarFile.add(path, os.path.basename(path))
+                    else:
+                        raise ValueError("%s does not exist" % path)
+
+            if self.getMakeValue("checksum", True):
+                with open(outTarPath, "rb") as outTarFile:
+                    md5sum = hashlib.md5(outTarFile.read()).hexdigest()
+
+                with open(outTarPath, "ab") as outTarFile:
+                    outTarFile.write(md5sum + "  " + os.path.basename(outTarPath) + "\n")
+
+                os.rename(outTarPath, checksummedOutTar)
+
+            resultTars.append(checksummedOutTar)
+
+
+        return resultTars
