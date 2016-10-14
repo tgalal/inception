525d64a2d9e917784a8e9777e38174da7d787e39
Tarek Galal <tarek.galal@suitepad.de>
2016-10-14 12:14:04 +0200

    More control over make flags
    --foo
    --no-foo
    --only-foo

--- inception/argparsers/make.py
+++ inception/argparsers/make.py
@@ -12,6 +12,7 @@ class MakeArgParser(InceptionArgParser):
 
     def __init__(self, description = "Make mode cmd"):
         super(MakeArgParser, self).__init__(description = description)
+        self.makeables = ["package", "boot", "cache", "recovery", "dnx", "update", "installer", "odin", "extras"]
 
         targetOpts = self.add_mutually_exclusive_group(required = True)
         targetOpts.add_argument('-v', '--variant',action = "store", help="variant config code to use, in the format A.B.C")
@@ -23,6 +24,18 @@ class MakeArgParser(InceptionArgParser):
         optionalOpts.add_argument("-w", "--keep-work", action="store_true", help="Don't delete work dir when finished")
         optionalOpts.add_argument('--learn-settings', action="store_true",
                                   help= "Learn settings from a connected device, and use in generated update package")
+
+        makeOpts = self.add_argument_group("General Make opts")
+
+        for makeable in self.makeables:
+            formattedMakeableName = makeable[0].upper() + makeable[1:]
+            makeOpts.add_argument("--only-%s" % makeable, action="store_true", help="Make only %s " % formattedMakeableName)
+
+            makeableGp = self.add_argument_group("%s Maker Opts" % formattedMakeableName)
+            flagGp = makeableGp.add_mutually_exclusive_group(required = False)
+            flagGp.add_argument("--%s" % makeable, dest=makeable, action="store_true", help="Make %s, overrides config" % makeable, default=None)
+            flagGp.add_argument("--no-%s" % makeable, dest=makeable, action="store_false", help = "Don't make %s, overrides config " % makeable, default=None)
+
 
         self.deviceDir = InceptionConstants.VARIANTS_DIR
         self.baseDir = InceptionConstants.BASE_DIR
@@ -63,11 +76,24 @@ class MakeArgParser(InceptionArgParser):
             sys.stderr.write("You are using an outdated config tree. Please run 'incept sync -v VARIANT_CODE' or set __config__ (see https://goo.gl/aFWPby)\n")
             sys.exit(1)
 
+        makeOnly = []
+        for makeable in self.makeables:
+            if self.args["only_%s" % makeable]:
+                makeOnly.append(makeable)
+
+        for makeable in self.makeables:
+            if len(makeOnly):
+                self.config.set("%s.__make__" % makeable, makeable in makeOnly)
+            elif self.args[makeable] is not None:
+                self.config.set("%s.__make__" % makeable, self.args[makeable])
+
+
 
         if outDir:
             self.config.setOutPath(outDir, self.args["keep_dirs"])
         else:
             outDir = self.config.getOutPath()
+
 
         self.workDir = self.getWorkDir()
         self.configDir = os.path.dirname(self.config.getSource())
