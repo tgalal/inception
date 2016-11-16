5f923a9e1add0b00a15d0e6de3fa4190e1a06c23
Tarek Galal <tarek.galal@suitepad.de>
2016-11-16 15:22:11 +0100

    Allow auto cache size

--- inception/argparsers/makers/maker_cache.py
+++ inception/argparsers/makers/maker_cache.py
@@ -12,12 +12,17 @@ class CacheMaker(Maker):
     def make(self, workDir, outDir):
 
         key, make_ext4fsBin = self.getHostBinary("make_ext4fs")
+        updatePkgPath = os.path.join(outDir, InceptionConstants.OUT_NAME_UPDATE)
         assert make_ext4fsBin, "must set %s to be able to create the cache img" % key
         assert os.path.exists(make_ext4fsBin), \
             "%s does not exist, please update %s to the correct path" % (make_ext4fsBin, key)
 
-        cacheSize = self.config.getMountConfig("cache.size", 33554432)
-        assert cacheSize, "__config__.target.mount.cache.size is not set, can't create cache img"
+        cacheSize = self.config.getMountConfig("cache.size", "auto")
+        if type(cacheSize) is not int or cacheSize <= 0:
+            cacheSize = os.path.getsize(updatePkgPath)
+            cacheSize = int(cacheSize / (1024 * 1024)) + 10 #safe offset to not fail?
+            cacheSize = "%sM" % cacheSize
+
         cacheSparsed= self.getMakeValue("sparsed", False)
         cacheMount = self.config.getMountConfig("cache.mount", "/cache")
         cacheMount = cacheMount[1:] if cacheMount[0] == "/" else cacheMount
@@ -28,7 +33,6 @@ class CacheMaker(Maker):
         gen.setSize(cacheSize)
         gen.setSparsed(cacheSparsed)
         gen.setMountPoint(cacheMount)
-        updatePkgPath = os.path.join(outDir, InceptionConstants.OUT_NAME_UPDATE)
         if os.path.exists(updatePkgPath):
             gen.update(updatePkgPath)
 
